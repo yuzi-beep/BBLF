87eead2b6ced7179af34e654db2cd06b797c4317
yuzi <19961257205@163.com>
2026-02-11 21:45:36 +0800

    refactor: implement singleton pattern for Supabase client in makeStaticClient

--- src/lib/shared/supabase.ts
+++ src/lib/shared/supabase.ts
@@ -2,9 +2,12 @@ import { createClient } from "@supabase/supabase-js";
 
 import { Database } from "@/types/supabase";
 
+let client: ReturnType<typeof createClient<Database>> | undefined;
+
 export function makeStaticClient() {
-  return createClient<Database>(
+  if (client) return client;
+  return (client = createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+  ));
 }
