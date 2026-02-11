15a7901ae3446bb20ebcf7c43d506e5496083fdc
yuzi <19961257205@163.com>
2026-02-11 21:30:55 +0800

    refactor: remove unused Profile type from index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,6 @@ import { Tables, TablesInsert, TablesUpdate } from "./supabase";
 export type Post = Tables<"posts">;
 export type Thought = Tables<"thoughts">;
 export type Event = Tables<"events">;
-export type Profile = Tables<"profiles">;
 
 export type PostInsert = TablesInsert<"posts">;
 export type PostUpdate = TablesUpdate<"posts">;
